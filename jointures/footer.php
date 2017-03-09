304a6b30d442f090e59f14691ebb4cb47edd5db0
Sabri MJAHED <sabri.mjahed.sio@gmail.com>
2017-03-09 17:36:18 +0100

    archi dossier + menu

---
+++ jointures/footer.php
@@ -0,0 +1,13 @@
+        <script src="//ajax.googleapis.com/ajax/libs/jquery/1.11.2/jquery.min.js"></script>
+        <script>window.jQuery || document.write('<script src="<?php echo $url; ?>js/vendor/jquery-1.11.2.min.js"><\/script>')</script>
+
+        <script src="<?php echo $url; ?>js/main.js"></script>
+
+
+        <?php
+            require_once('../jointures/modals.php');
+        ?>
+
+
+    </body>
+</html>
