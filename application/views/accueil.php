bac478b13bf381c84701ab0b2bee07f405762556
AntoineSio <antoine.espinosa.sio@gmail.com>
2017-03-17 22:12:00 +0000

    Mise en place de CI

---
+++ application/views/accueil.php
@@ -0,0 +1,11 @@
+<html>
+	<head>
+		<title>Accueil Noirmoutier
+		</title>
+	</head>
+	<body>
+       	<div class="row">
+              <center><h1>Page Accueil</h1></center>
+          </div>
+      </div>
+
