1ab5244014b1798ef98a53d7436934a961ba5b70
AntoineSio <antoine.espinosa.sio@gmail.com>
2017-03-24 16:15:55 +0000

    Ajout de Roboto

--- application/views/jointures/header.php
+++ application/views/jointures/header.php
@@ -1,5 +1,6 @@
 <?php
-    $url = base_url('application/assets/');
+	$urlassets = base_url('application/assets/');
+	$urlsite = base_url('application/views/');
 ?>
 <!doctype html>
 <!--[if lt IE 7]>      <html class="no-js lt-ie9 lt-ie8 lt-ie7" lang=""> <![endif]-->
@@ -14,12 +15,12 @@
         <meta name="viewport" content="width=device-width, initial-scale=1">
         <link rel="apple-touch-icon" href="apple-touch-icon.png">
 
-        <link rel="stylesheet" href="<?php echo $url; ?>css/normalize.min.css">
-        <link rel="stylesheet" href="<?php echo $url; ?>css/materialize.css">
+        <link rel="stylesheet" href="<?php echo $urlassets; ?>css/normalize.min.css">
+        <link rel="stylesheet" href="<?php echo $urlassets; ?>css/materialize.css">
         <link href="https://fonts.googleapis.com/icon?family=Material+Icons" rel="stylesheet">
-        <link rel="stylesheet" href="<?php echo $url; ?>css/main.css">
+        <link rel="stylesheet" href="<?php echo $urlassets; ?>css/main.css">
 
-        <script src="<?php echo $url; ?>js/vendor/modernizr-2.8.3-respond-1.4.2.min.js"></script>
+        <script src="<?php echo $urlassets; ?>js/vendor/modernizr-2.8.3-respond-1.4.2.min.js"></script>
     </head>
     <body>
         <!--[if lt IE 8]>
@@ -30,8 +31,8 @@
         <div class="nav-wrapper">
         <a href="#!" class="brand-logo">Logo</a>
         <ul class="right hide-on-med-and-down">
-            <li><a href="<?php echo $url; ?>accueil"><i class="material-icons left">home</i>Acceuil</a></li>
-            <li><a href="<?php echo $url; ?>recherche"><i class="material-icons left">search</i>Stage</a></li>
+            <li><a href="<?php echo $urlsite; ?>accueil.php"><i class="material-icons left">home</i>Acceuil</a></li>
+            <li><a href="<?php echo $urlsite; ?>recherche.php"><i class="material-icons left">search</i>Stage</a></li>
             <li><a class="waves-effect waves-light btn modal-trigger" data-target="#modalsign" href="#modalsign">Login/Sign up</a></li>
         </ul>
         </div>
