b9e81e15fc8eb65aa943377ec0f224b52e2433ad
renko174 <thomas.goislard.sio@gmail.com>
2017-05-12 08:59:59 +0200

    ajoue inscraption et connection

--- application/controllers/Accueil.php
+++ application/controllers/Accueil.php
@@ -18,12 +18,51 @@ class Accueil extends CI_Controller {
 	 * map to /index.php/welcome/<method_name>
 	 * @see https://codeigniter.com/user_guide/general/urls.html
 	 */
-	public function index()
-	{
+	public function index(){
 		$this->load->view('jointures/header');
 		$this->load->view('accueil');
 		$this->load->view('jointures/footer');
 		$this->load->view('jointures/modals');
 	}
+	public function connection() {
+            $mail = $this->input->get('mail');
+            $mdp = $this->input->get('mdp');
+
+            $resultat = $this->Noimoutier_model->connectionCitoyen($mail,$mdp);
+            if($resultat==TRUE){
+                $nom = $this->Noimoutier_model->getNomAdhrent($mail);
+                start_session ;
+                $_SESSION['Auth']['nom']=$nom;
+                $this->load->view('accueil.php');
+            }else{
+                
+            }
+        }
+        
+        public function inscription(){
+        
+		$nom=$this->input->post('nom');
+		$prenom=$this->input->post('prenom');
+		$mail=$this->input->post('mail');
+		$mdp=$this->input->post('mdp');
+		$num=$this->input->post('numRue');
+		$adresse=$this->input->post('adresse');
+		$dept=$this->input->post('dept');
+		$commune=$this->input->post('commune');
+
+		$sport=$this->input->post('sport');
+		$niveau=$this->input->post('niveau');
+
+
+		$newCitoyen=$this->noirmoutier_model->addCitoyen($nom,$prenom,$mail,$mdp,$num,$adresse,$dept,$commune);
+		$newPratiquer=$this->noirmoutier_model->setPratiquer($sport,$mail,$nom,$prenom,$niveau);
+
+		if($newCitoyen && $newPratiquer){
+		    $this->load->view('Homepage');
+
+		}else{
+		     $this->load->view('vueAdhesionSite');
+		}
+   	 }
 }
 
